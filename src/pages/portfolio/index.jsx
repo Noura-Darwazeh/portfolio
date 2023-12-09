2744fe1613bcbc8d6ff8cb373b637dc6de8e2c64
Noura-Darwazeh <noura.darwazeh11@gmail.com>
2023-12-09 14:46:06 -0500

    add my projects

--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PageHeaderContent from '../../components/pageHeaderContent';
 import ImageOne from '../../assets/image/E-Commerce.png'
+import ImageTwo from '../../assets/image/Salonati.jpg'
 import './styles.scss';
 
 const projects = [{
@@ -9,52 +10,22 @@ const projects = [{
   name: 'E-Commerce',
   date: 10 / 2023 - 11 / 2023,
   description: 'This is a front-end e-commerce website using React js with an easy-to-use interface for item management, browsing, and purchases. Explore, shop, log in, add items to their wishlists and carts, check item details, and place orders are all available to users.',
-  link:'https://github.com/JSD-0923/frontend-final-2',
+  link: 'https://github.com/JSD-0923/frontend-final-2',
 },
 {
-  id: 1,
-  image: ImageOne,
-  name: 'E-Commerce',
+  id: 2,
+  image: ImageTwo,
+  name: 'Salonati',
   date: 10 / 2023 - 11 / 2023,
-  description: 'This is a front-end e-commerce website using React js with an easy-to-use interface for item management, browsing, and purchases. Explore, shop, log in, add items to their wishlists and carts, check item details, and place orders are all available to users.',
-  link:'https://www.youtube.com/watch?v=RGzs7T75_0o',
+  description: 'Achieved a 90% score on this project. I implemented a mobile app and website for booking an appointment in a specific beauty center using Flutter, Node js',
+  link: 'https://github.com/Noura-Darwazeh/Graduation-project',
 
 },
-
-{
-  id: 1,
-  image: ImageOne,
-  name: 'E-Commerce',
-  date: 10 / 2023 - 11 / 2023,
-  description: 'This is a front-end e-commerce website using React js with an easy-to-use interface for item management, browsing, and purchases. Explore, shop, log in, add items to their wishlists and carts, check item details, and place orders are all available to users.',
-  link:'https://www.youtube.com/watch?v=rbck0aR7HiA&list=PL0JfpSrXxTMyybivS1O2RPQw6V173EaWT&index=15',
-
-},
-
-{
-  id: 1,
-  image: ImageOne,
-  name: 'E-Commerce',
-  date: 10 / 2023 - 11 / 2023,
-  description: 'This is a front-end e-commerce website using React js with an easy-to-use interface for item management, browsing, and purchases. Explore, shop, log in, add items to their wishlists and carts, check item details, and place orders are all available to users.',
-  link:'https://www.youtube.com/watch?v=rbck0aR7HiA&list=PL0JfpSrXxTMyybivS1O2RPQw6V173EaWT&index=15',
-
-},
-
-{
-  id: 1,
-  image: ImageOne,
-  name: 'E-Commerce',
-  date: 10 / 2023 - 11 / 2023,
-  description: 'This is a front-end e-commerce website using React js with an easy-to-use interface for item management, browsing, and purchases. Explore, shop, log in, add items to their wishlists and carts, check item details, and place orders are all available to users.',
-  link:'https://www.youtube.com/watch?v=rbck0aR7HiA&list=PL0JfpSrXxTMyybivS1O2RPQw6V173EaWT&index=15',
-
-}
 
 ]
 const Projects = () => {
-  const [hoveredValue , setHoveredValue] = useState(null)
-  function handleHover(index){
+  const [hoveredValue, setHoveredValue] = useState(null)
+  function handleHover(index) {
     setHoveredValue(index);
   }
   return (
@@ -65,8 +36,8 @@ const Projects = () => {
           {
             projects.map((item, index) => (
               <div className='projects__content__cards__item' index={item.id}
-              onMouseEnter={()=>handleHover(index)}
-              onMouseLeave={()=>handleHover(null)}
+                onMouseEnter={() => handleHover(index)}
+                onMouseLeave={() => handleHover(null)}
               >
                 <div className='projects__content__cards__item__img-wrapper'>
                   <a href={item.link}>
@@ -75,21 +46,21 @@ const Projects = () => {
                 </div>
                 <div className='projects__content__cards__item__description'>
                   <h3>{item.name}</h3>
-                  <br/>
+                  <br />
                   <p>{item.description}</p>
-                  </div>
+                </div>
 
-                  <div className='overlay'>
-                    {
-                      index === hoveredValue && (
-                        <div>
-                          <a href={item.link}>
+                <div className='overlay'>
+                  {
+                    index === hoveredValue && (
+                      <div>
+                        <a href={item.link}>
                           <button> View </button>
-                          </a>
-                        </div>
-                      )
-                    }
-                  </div>
+                        </a>
+                      </div>
+                    )
+                  }
+                </div>
               </div>
             ))
           }
