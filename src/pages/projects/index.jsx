06cef5ace3be3a6cd446a5659a30efa342113917
Noura-Darwazeh <noura.darwazeh11@gmail.com>
2023-12-30 18:38:47 -0500

    deploy after add one project

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PageHeaderContent from '../../components/pageHeaderContent';
 import ImageOne from '../../assets/image/E-Commerce.png'
 import ImageTwo from '../../assets/image/Salonati.jpg'
+import ImageThree from '../../assets/image/Weather-App.PNG'
 import './styles.scss';
 
 const projects = [{
@@ -10,7 +11,7 @@ const projects = [{
   name: 'E-Commerce',
   date: 10 / 2023 - 11 / 2023,
   description: 'This is a front-end e-commerce website using React js with an easy-to-use interface for item management, browsing, and purchases. Explore, shop, log in, add items to their wishlists and carts, check item details, and place orders are all available to users.',
-  link: 'https://github.com/JSD-0923/frontend-final-2',
+  link: 'https://github.com/Noura-Darwazeh/frontend-final-2',
 },
 {
   id: 2,
@@ -19,6 +20,15 @@ const projects = [{
   date: 10 / 2023 - 11 / 2023,
   description: 'Achieved a 90% score on this project. I implemented a mobile app and website for booking an appointment in a specific beauty center using Flutter, Node js',
   link: 'https://github.com/Noura-Darwazeh/Graduation-project',
+
+},
+{
+  id: 3,
+  image: ImageThree,
+  name: 'Weather App',
+  date: 12 / 2023,
+  description: 'Implemented weather website using react js , when user enter city name weather information for this city is displayed, such as temperature, wind speed, etc',
+  link: 'https://github.com/Noura-Darwazeh/weather-app-using-react',
 
 },
 
